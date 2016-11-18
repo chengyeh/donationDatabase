b56b5475ba97463e8b8a1e14042ac0a8c02576ae
Dustin Horvath <dustinmhorvath@gmail.com>
2016-11-18 12:02:27 -0600

    Initial commit of install script. Nothing fancy, just a framework.

---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Exit on any error non-zero
+set -e
+
+# You need to be root, sorry.
+if [[ $EUID -ne 0 ]]; then
+echo "This script requires elevated privileges to run. Are you root?"
+exit
+fi
+
+echo 'Create MySQL root password:'
+read -s MYSQLROOTPASSWD
+
+DATE=$(date +"%Y%m%d%H%M")
+
+# Pre-populate the root password for mysql-server before install
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQLROOTPASSWD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQLROOTPASSWD"
+
+# Get mysql-server core
+apt-get -y install mysql-server
+
+MAINDB="donation"
+RESULT=`mysqlshow --user=root --password=${MYSQLROOTPASSWD} $MAINDB | grep -v Wildcard | grep -o $MAINDB`
+if [ "$RESULT" == "$MAINDB" ]; then
+echo "Database $MAINDB already exists"
+else
+echo "Database not found, creating..."
+mysql -uroot -p${MYSQLROOTPASSWD} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+mysql -uroot -p${MYSQLROOTPASSWD} -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
+mysql -uroot -p${MYSQLROOTPASSWD} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+mysql -uroot -p${MYSQLROOTPASSWD} -e "FLUSH PRIVILEGES;"
+fi
+
+echo "Creating DonorTable..."
+mysql -uroot -p${MYSQLROOTPASSWD} $MAINDB -e "
+CREATE TABLE DonorTable(
+		DonorID INT NOT NULL AUTO_INCREMENT,
+		FirstName VARCHAR(24) NOT NULL,
+		LastName VARCHAR(24) NOT NULL,
+		Telephone INT(10),
+		Email VARCHAR(50) NOT NULL,
+		Address VARCHAR(50),
+		PRIMARY KEY ( DonorID )
+		);"
+
+	echo "Creating DoneeTable..."
+	mysql -uroot -p${MYSQLROOTPASSWD} $MAINDB -e "
+CREATE TABLE DoneeTable (
+		DoneeID INT NOT NULL AUTO_INCREMENT,
+		FirstName VARCHAR(24) NOT NULL,
+		LastName VARCHAR(24) NOT NULL,
+		Telephone INT(10),
+		Email VARCHAR(50) NOT NULL,
+		Address VARCHAR(50),
+		Age INT,
+		Gender VARCHAR(6),
+		Ethnicity VARCHAR(24),
+		PRIMARY KEY ( DoneeID )
+		);"
