a8229725be27f01a347ea802d5dec2e8afaa0f44
Taylor Hockersmith <ynimos@gmail.com>
2017-02-01 08:49:27 -0600

    Adding debugging to config.php

--- config.php
+++ config.php
@@ -1,2 +1,38 @@
 <?php
 $config = parse_ini_file(__DIR__.'./config.ini');
+
+
+if (basename($_SERVER['REQUEST_URI']) === 'config.php') {
+    $dir = __DIR__;
+    $path_web = $config['path_web'];
+    $link = $path_web . 'html/';
+    $homelink = $link . 'index.php';
+    $csslink = $link . 'bootstrap/css/custom.css';
+
+    $protocol = 'http';
+    if (isset($_SERVER['HTTPS']))
+        $protocol .= 's';
+    $server_name = $_SERVER['SERVER_NAME'];
+    $folder = substr(__DIR__, strlen($_SERVER['DOCUMENT_ROOT']) + 1);
+    $autopath = "$protocol://$server_name/$folder/";
+
+    echo("<h3>Config.php debug info</h3>");
+    echo("Dir: $dir<br />");
+    echo("Path: $path_web<br />");
+    echo("Suggested path: $autopath<br/>");
+    echo("Home link: <a href=\"$homelink\">$homelink</a><br />");
+    echo("CSS link: <a href=\"$csslink\">$csslink</a><br />");
+
+    echo("<hr>");
+
+    if ($config['path_web'] === '') {
+        die("<span style=\"color:red\">Error detected: path_web in config.ini is empty</span><br />");
+    } else {
+        $lastchar = substr($path_web, -1);
+        if ($lastchar !== '/')
+            echo("Problem detected: path_web in config.ini does not end in /<br />");
+    }
+} else if ($config['path_web'] === '') {
+    die("<span style=\"color:red\">Error detected: path_web in config.ini is empty</span><br />");
+}
+
