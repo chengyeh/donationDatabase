877161eb225cba2aa892d5a5abc3e75b253fa66b
Taylor Hockersmith <ynimos@gmail.com>
2017-02-06 08:38:16 -0600

    Improving config.php debugging
    No longer always reports that the protocol is incorrect, and should now
    suggest the correct path on the cycle servers

--- config.php
+++ config.php
@@ -13,8 +13,9 @@ if (basename($_SERVER['REQUEST_URI']) === 'config.php') {
 	if (isset($_SERVER['HTTPS']))
 		$protocol .= 's';
 	$server_name = $_SERVER['SERVER_NAME'];
+	$prefix = $_SERVER['CONTEXT_PREFIX'];
 	$folder = substr(__DIR__, strlen($_SERVER['CONTEXT_DOCUMENT_ROOT']) + 1);
-	$autopath = "$protocol://$server_name/$folder/";
+	$autopath = "$protocol://$server_name$prefix/$folder/";
 
 	echo('<h3>Config.php debug info</h3>');
 	echo("Dir: $dir<br />");
@@ -28,7 +29,7 @@ if (basename($_SERVER['REQUEST_URI']) === 'config.php') {
 	if ($config['path_web'] === '') {
 		die('<span style=\"color:red\">Error detected: path_web in config.ini is empty</span><br />');
 	} else {
-		$config_protocol = substr($path_web, strlen($protocol));
+		$config_protocol = substr($path_web, 0, strlen($protocol));
 		if ($config_protocol !== $protocol)
 			echo("Problem detected: path_web in config.ini using incorrect protocol (recommended: $protocol)<br />");
 		$lastchar = substr($path_web, -1);
@@ -38,4 +39,4 @@ if (basename($_SERVER['REQUEST_URI']) === 'config.php') {
 } else if ($config['path_web'] === '') {
 	die('<span style=\"color:red\">Error detected: path_web in config.ini is empty</span><br />');
 }
-
+//phpinfo();
