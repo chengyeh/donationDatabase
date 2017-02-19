42e32cbeadf478db4bc4ed5a6e2f21612ecae844
Michael Bechtel <m783b224@ku.edu>
2017-02-18 21:21:18 -0600

    Revert "Update donate page to match new database tables"
    This reverts commit d81fa31fabd67ea2ab96c8bfb7a16fb23b3f8437.

--- html/donor.php
+++ html/donor.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang = "en">
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
 session_start();
 
@@ -9,14 +11,22 @@ $navbar_title = 'Donor Page';
 include('layouts/navbar.php');
 require_once('helpers/mysqli.php');
 
-$item = mysqli_real_escape_string($mysqli, $_GET["request"]);
+    
+$sql = "SELECT * FROM CategoriesTable";
+$result_set = $mysqli->query($sql);
+$category_array = array();
+while($row =  mysqli_fetch_array($result_set)){
+     $category_array[] = $row;
+}
+
+/*$item = mysqli_real_escape_string($mysqli, $_GET["request"]);
 $amount = mysqli_real_escape_string($mysqli, $_GET["first"]);
 
 if(isset($_SESSION["id"]))
 {
 	$temp = $_SESSION["id"];
 
-	$updateAmount = "INSERT INTO IncDonationTable (DonorID, Amount, PledgeDate) VALUES ($temp, '$amount', NOW())";
+	$updateAmount = "INSERT INTO dd_indonation (donor_id, amount, date_pledged) VALUES ($temp, '$amount', NOW())";
 
 	if($r = $mysqli->query($updateAmount))
 	{
@@ -24,34 +34,48 @@ if(isset($_SESSION["id"]))
 	}
 
 	$mysqli->close();
-}
+}*/
 ?>
 
 <div class="container">
 	<h3>Item Donation Form</h3> <br>
 	<form action="donor.php">
-		<table class="table table-striped">
-
-			<tr>
-				<th>Item</th>
-				<th>Category</th>
-				<th>Quantity</th>
-			</tr>
-
-			<tr>
-				<td>
-				<select name='request'>
-					<option selected>Select item</option>
-					<option>Shirts</option>
-					<option>Shorts</option>
-					<option>Food item 1</option>
-					<option>Food item 2</option>
-				</select>
-				</td>
-				<td>{Autopopulate category here}</td>
-				<td><input type="number" value="item1" name="first" min="0" scale="1"></td>
-			</tr>
-		</table>
+		<?php
+		 foreach($category_array as $category)
+		 {
+		 	echo '<div class="panel-group">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a data-toggle="collapse" href="#cat' . $category['CategoryNum'] . '">' . $category['Name'] . '</a>
+							</h4>
+						</div>
+						<div id="cat' . $category['CategoryNum'] . '" class="panel-collapse collapse">
+							<div class="panel-body">
+								<table class="table table-striped">
+									<tr>
+										<th>Item</th>
+										<th>Need</th>
+										<th>Quantity</th>
+									</tr>';
+									
+			$sql = "SELECT * FROM InventoryTable WHERE CategoryNum =" . $category['CategoryNum'] . " AND Amount != Threshold";
+			$result_set = $mysqli->query($sql);
+			$inventory_array = array();
+			while($row =  mysqli_fetch_array($result_set))
+			{
+				$inventory_array[] = $row;
+			}
+			
+			foreach($inventory_array as $item)
+			{
+				echo '<tr><td>' . $item['Name'] . '</td><td>' . ($item['Threshold']-$item['Amount']) . '</td><td><input type="number" value="item1" name="first" min="0" scale="1"></td></tr>';
+			}
+			echo '</table></div></div></div></div>';
+		 	 	
+		 }
+		?>
+		
 		<hr>
 		<div class="form-group">
 			<label for="specialRequests" class="col-sm-2 control-label">Special Donations</label>
