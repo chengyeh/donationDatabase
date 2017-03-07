57148ffb2b7fa2d6469012ed8479da1dee97bcbe
Michael Bechtel <m783b224@ku.edu>
2017-03-06 20:25:52 -0600

    Add item category to donor livesearch

--- html/donor.php
+++ html/donor.php
@@ -97,7 +97,7 @@ if(isset($_GET["input0"]) && isset($_SESSION["id"]))
 				$.post('invSearch.php', {keywords: substr}, function(data) {
 					$('ul#results').empty();
 					$.each(data, function() {
-						$('ul#results').append('<li>' + this.name + '</li>');
+						$('ul#results').append('<li>' + this.name + ' can be found in ' + this.category + '</li>');
 					});
 				}, "json");
 			}
