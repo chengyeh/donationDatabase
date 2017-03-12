ab8fe906e07fd6966330e1cb05c70637389f6e8c
Dustin Horvath <dustinmhorvath@gmail.com>
2017-03-12 17:49:44 -0500

    Cleaned up format slightly.

--- html/donor.php
+++ html/donor.php
@@ -165,6 +165,8 @@ $(document).ready(function(){
 		<input type="submit" value="Pledge Donation">
 	</form>
 
+	<br> <h3>Donate using PayPal</h3> <br>
+
 	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 	<input type="hidden" name="cmd" value="_s-xclick">
 	<input type="hidden" name="hosted_button_id" value="FZE3S3J459SF2">
@@ -172,8 +174,8 @@ $(document).ready(function(){
 	<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 	</form>
 
-
 </div>
+
 
 </body>
 </html>
