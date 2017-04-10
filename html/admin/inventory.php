4e335fae7e7353a2df7df9d35344cc991e01bd9c
Cheng Yeh Lee <chengyeh@ku.edu>
2017-04-09 20:51:30 -0500

    Can now view item information from the inventory table. Admin will be able to edit and delete items

--- html/admin/inventory.php
+++ html/admin/inventory.php
@@ -1,7 +1,80 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+session_start();
+
+$navbar_active = 'admin';
+$navbar_title = 'Admin panel - Inventory';
+include('../layouts/navbar.php');
+require_once('../helpers/mysqli.php');
+
+$id = '';
+
+if (isset($_SESSION['id']) && $_SESSION['admin'] && $_SESSION['active'])
+{
+	$id = $_SESSION['id'];
+}
+else
+{
+	if (!isset($_SESSION['id']) || !$_SESSION['active']) {
+		$path = $config['path_web'] . 'html/login.php';
+		$err = 401;
+		header("Location:$path?err=$err&dest=donee");
+	} else { // !$_SESSION['admin']
+		$path = $config['path_web'] . 'html/profile.php';
+		$err = 5;
+		header("Location:$path?err=$err&dest=donor");
+	}
+	exit;
+}
+?>
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	//If the edit button is clicked, update the item amount and threshold
+	if($_POST['action'] == 'edit')
+	{	
+	    $sql = "UPDATE InventoryTable SET Amount = " . $_POST['amount'] . ", Threshold = " . $_POST['threshold'] . " WHERE ItemID = " . $_POST['item_id'];
+	    $mysqli->query($sql);
+	}
+	
+	// If the delete button is clicked, delete the item from the inventory table
+    if($_POST['action'] == 'delete')
+    {
+    	$sql = "DELETE FROM InventoryTable WHERE ItemID =" . $_POST['item_id'];
+    	$mysqli->query($sql);
+    }
+}
+?>
+<?php
+//Query all the rows from the table
+function get_rows($table_name, $order)
+{
+	global $mysqli;
+	$sql = "SELECT * FROM " . $table_name . " ORDER BY " . $order . " ASC";
+	$result_set = $mysqli->query($sql);
+	$object_array = array();
+	while($row =  mysqli_fetch_array($result_set))
+	{
+    	$object_array[] = $row;
+	}
+	return $object_array;
+}
+
+//Returns the count of rows from the table
+function count_all($table_name)
+{
+	global $mysqli;
+    $sql = "SELECT COUNT(*) FROM " . $table_name;
+    $result_set = $mysqli->query($sql);
+    $row = mysqli_fetch_array($result_set);
+    return array_shift($row);
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Inventory | JayHawk Charity Admin</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -22,47 +95,6 @@
         <![endif]-->
     </head>
     <body class="bootstrap-admin-with-small-navbar">
-        <!-- small navbar -->
-        <nav class="navbar navbar-default navbar-fixed-top bootstrap-admin-navbar-sm" role="navigation">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="collapse navbar-collapse">
-                            <ul class="nav navbar-nav navbar-right">                              
-                                <li>
-                                    <a href="#">Go to frontend <i class="glyphicon glyphicon-share-alt"></i></a>
-                                </li>
-                                <li class="dropdown">
-                                    <a href="#" role="button" class="dropdown-toggle" data-hover="dropdown"> <i class="glyphicon glyphicon-user"></i> Cheng-Yeh <i class="caret"></i></a>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="">Logout</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </nav>
-
-        <!-- main / large navbar -->
-        <nav class="navbar navbar-default navbar-fixed-top bootstrap-admin-navbar bootstrap-admin-navbar-under-small" role="navigation">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".main-navbar-collapse">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                            <a class="navbar-brand" href="index.html">Admin Panel</a>
-                        </div>
-                    </div>
-                </div>
-            </div><!-- /.container -->
-        </nav>
 
         <div class="container">
             <!-- left, vertical navbar & content -->
@@ -74,16 +106,16 @@
                             <a href="index.php"><i class="glyphicon glyphicon-chevron-right"></i> Recently Added</a>
                         </li>
                         <li>
-                            <a href="users.php"><span class="badge pull-right"> 6 </span> Users</a>
+                            <a href="users.php"><span class="badge pull-right"><?php echo count_all("UserTable"); ?></span> Users</a>
                         </li>
                         <li class="active">
-                            <a href="inventory.php"><span class="badge pull-right"> 4 </span> Inventory</a>
+                            <a href="inventory.php"><span class="badge pull-right"><?php echo count_all("InventoryTable"); ?></span> Inventory</a>
                         </li>
                         <li>
-                            <a href="incDonation.php"><span class="badge pull-right"> 4 </span> Incoming Donation</a>
+                            <a href="incDonation.php"><span class="badge pull-right"><?php echo count_all("IncDonationTable"); ?></span> Incoming Donation</a>
                         </li>
                         <li>
-                            <a href="outDonation.php"><span class="badge pull-right"> 20 </span> Outgoing Donation</a>
+                            <a href="outDonation.php"><span class="badge pull-right"><?php echo count_all("OutDonationTable"); ?></span> Outgoing Donation</a>
                         </li>
                     </ul>
                 </div>
@@ -93,7 +125,7 @@
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="page-header">
-                                <h1>Inventory<div class="pull-right"><input type="submit" id="button" class="btn btn-sm btn-primary"  name="action" value="Generate Report"></div></h1>
+                                <h1>Inventory<div class="pull-right"><form action="add_item.php"><input type="submit" class="btn btn-success" value="Insert new item" /></form></div></h1>
                             </div>
                         </div>
                     </div>
@@ -102,83 +134,47 @@
                             <div class="panel panel-default">
                                 <div class="panel-heading">
                                     <div class="text-muted bootstrap-admin-box-title">Inventory</div>
-                                    <div class="pull-right"><span class="badge"> 20 </span></div>
+                                    <div class="pull-right"><span class="badge"><?php echo count_all("InventoryTable"); ?></span></div>
                                 </div>
-                                <div class="bootstrap-admin-panel-content">                               
+                                <div class="bootstrap-admin-panel-content">                                                             
                                     <table class="table bootstrap-admin-table-with-actions">
                                         <thead>
                                             <tr>
                                                 <th>Item ID</th>
                                                 <th>Name</th>
+                                                <th>Category</th>
                                                 <th>Amount</th>
                                                 <th>Threshold</th>
-                                                <th>Request</th>
                                                 <th>Actions</th>
                                             </tr>
                                         </thead>
                                         <tbody>
+                                        <?php
+                                        $inventory_array = get_rows("InventoryTable", "ItemID");
+										foreach($inventory_array as $item)
+										{ 
+											echo "<form action='' method='POST'>";
+											$sql = "SELECT Name FROM CategoriesTable WHERE CategoryNum =" . $item['CategoryNum'];
+												$result_set = $mysqli->query($sql);
+												$category = array();
+												while($row =  mysqli_fetch_array($result_set)){
+												     $category[] = $row;
+												}
+										?>				
                                         	<tr>
-                                                <td>150</td>
-                                                <td>Towel</td>
-                                                <td><input type="number" name="amount"  value="54"></td>
-                                                <td><input type="number" name="threshold" value="200"></td>
+                                                <td><?php echo $item['ItemID']; ?></td>
+                                                <td><?php echo $item['Name']; ?></td>
+                                                <td><?php echo $category[0]['Name']; ?></td>
+                                                <td><input type="number" name="amount"  value="<?php echo $item['Amount']; ?>"></td>
+                                                <td><input type="number" name="threshold" value="<?php echo $item['Threshold']; ?>"></td>
                                                 <td>
-                                                	<select name='request'>
-                                                		<option selected>Pending request</option>
-                                                		<option>Docia Bobby (5)</option>
-                                                		<option>Edison Averill (7)</option>
-                                                	</select>
-												</td>
-                                                <td>
-                                                	<input type="submit" id="button" class="btn btn-sm btn-primary"  name="action" value="Edit">
+                                                	<input type="hidden" name="item_id" value="<?php echo $item['ItemID']; ?>">
+                                                	<button type="submit" class="btn btn-sm btn-primary" name="action" value="edit">Edit</button>
+                                                	<button type="submit" class="btn btn-sm btn-danger" name="action" value="delete">Deletet</button>
 	                                            </td>
-                                            </tr>
-                                            <tr>
-                                                <td>231</td>
-                                                <td>Computer</td>
-                                                <td><input type="number" name="amount"  value="12"></td>
-                                                <td><input type="number" name="threshold" value="30"></td>
-                                                <td>
-                                                	<select name='request'>
-                                                		<option selected>No request</option>
-                                                	</select>
-												</td>
-                                                <td>
-                                                	<input type="submit" id="button" class="btn btn-sm btn-primary"  name="action" value="Edit">
-	                                            </td>
-                                            </tr>
-                                            <tr>
-                                                <td>341</td>
-                                                <td>Pencil</td>
-                                                <td><input type="number" name="amount"  value="119"></td>
-                                                <td><input type="number" name="threshold" value="300"></td>
-                                                <td>
-                                                	<select name='request'>
-                                                		<option selected>Pending request</option>
-                                                		<option>Sheryll Idonea (10)</option>
-                                                		<option>Kristopher Jodene (5)</option>
-                                                	</select>
-												</td>
-                                                <td>
-                                                	<input type="submit" id="button" class="btn btn-sm btn-primary"  name="action" value="Edit">
-	                                            </td>
-                                            </tr>
-                                            <tr>
-                                                <td>256</td>
-                                                <td>Toothbrush</td>
-                                                <td><input type="number" name="amount"  value="54"></td>
-                                                <td><input type="number" name="threshold" value="200"></td>
-                                                <td>
-                                                	<select name='request'>
-                                                		<option selected>Pending request</option>
-                                                		<option>Beau Hammond (5)</option>
-                                                	</select>
-												</td>
-                                                <td>
-                                                	<input type="submit" id="button" class="btn btn-sm btn-primary"  name="action" value="Edit">                                       
-	                                            </td>
-                                            </tr>                                                                                     
-                                        </tbody>
+                                            </tr></form>
+                                        <?php } ?>                                            
+                                        </tbody>	                                       
                                     </table>  
                                 </div>
                             </div>
@@ -194,7 +190,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <footer role="contentinfo">
-                            <p class="right">&copy; 2016 <a href="#">JayHawk Charity</a></p>
+                            <p class="right">&copy; 2017 <a href="#">Donation database</a></p>
                         </footer>
                     </div>
                 </div>
