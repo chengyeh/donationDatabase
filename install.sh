f62790d915d8b0b275e353e3858f3e7278e90026
Dustin Horvath <dustinmhorvath@gmail.com>
2017-04-10 14:05:33 -0500

    Cleanup, comments.

--- install.sh
+++ install.sh
@@ -55,7 +55,7 @@ echo "Configuring mysql-server..."
 # Check if mysql-server installed, continue on non-0 and we handle the failure
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' mysql-server|grep "install ok installed") || true
 echo Checking for mysql-server: $PKG_OK
-# Pre-populate the root password for mysql-server before install, but only if not installed
+# Pre-populate the root password for mysql-server before install, but only if not installed.
 if [ "" == "$PKG_OK" ]; then
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQLROOTPASSWD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQLROOTPASSWD"
@@ -68,20 +68,24 @@ fi
 
 # Check if database exists
 RESULT=`mysqlshow --user=root --password=${MYSQLROOTPASSWD} $MAINDB | grep -v Wildcard | grep -o $MAINDB` || true;
+# Create database if not exists
 if [ "$RESULT" == "$MAINDB" ]; then
-echo "Database $MAINDB already exists"
+  echo "Database $MAINDB already exists"
 else
-echo "Database not found, creating..."
-mysql -uroot -p${MYSQLROOTPASSWD} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-if [ $(mysql -N -s -u root -p${MYSQLROOTPASSWD} -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$MAINDBUSER');") -eq 1 ]; then
-	echo "Creating privileged database user $MAINDBUSER..."
-	mysql -uroot -p${MYSQLROOTPASSWD} -e "CREATE USER ${MAINDBUSER}@localhost IDENTIFIED BY '${MYSQLROOTPASSWD}';"
+  echo "Database not found, creating..."
+  mysql -uroot -p${MYSQLROOTPASSWD} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 fi
+# Create database user if not exists
+if [ $(mysql -N -s -u root -p${MYSQLROOTPASSWD} -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '$MAINDBUSER');") -eq 1 ]; then
+  echo "Creating privileged database user $MAINDBUSER..."
+  mysql -uroot -p${MYSQLROOTPASSWD} -e "CREATE USER ${MAINDBUSER}@localhost IDENTIFIED BY '${MYSQLROOTPASSWD}';"
+fi
+# Grant privileges to database user
 echo "Granting required privileges..."
 mysql -uroot -p${MYSQLROOTPASSWD} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDBUSER}'@'%';"
 echo "Flushing..."
 mysql -uroot -p${MYSQLROOTPASSWD} -e "FLUSH PRIVILEGES;"
-fi
+
 
 
 ###### START CREATE DATABASE TABLES ######
@@ -91,17 +95,17 @@ if [ $(mysql -N -s -u root -p${MYSQLROOTPASSWD} -e "select count(*) from informa
 else
 	echo "Creating IncDonationTable..."
 	mysql -uroot -p${MYSQLROOTPASSWD} $MAINDB -e "
-CREATE TABLE IncDonationTable (
-	RefNum INT NOT NULL AUTO_INCREMENT,
-	DonorID INT NOT NULL,
-	ItemID VARCHAR(30) NOT NULL,
-	Amount INT NOT NULL,
+    CREATE TABLE IncDonationTable (
+    RefNum INT NOT NULL AUTO_INCREMENT,
+    DonorID INT NOT NULL,
+    ItemID VARCHAR(30) NOT NULL,
+    Amount INT NOT NULL,
     ActualAmount INT NOT NULL,
     Value INT NOT NULL,
     PledgeDate TIMESTAMP NOT NULL,
     ReceiveDate TIMESTAMP,
-		PRIMARY KEY ( RefNum )
-		);"
+	PRIMARY KEY ( RefNum )
+    );"
 fi
 
 if [ $(mysql -N -s -u root -p${MYSQLROOTPASSWD} -e "select count(*) from information_schema.tables where table_schema='$MAINDB' and table_name='OutDonationTable';") -eq 1 ]; then
@@ -109,14 +113,14 @@ if [ $(mysql -N -s -u root -p${MYSQLROOTPASSWD} -e "select count(*) from informa
 else
 	echo "Creating OutDonationTable..."
 	mysql -uroot -p${MYSQLROOTPASSWD} $MAINDB -e "
-CREATE TABLE OutDonationTable (
-	RefNum INT NOT NULL,
-	DoneeID INT NOT NULL,
-	ItemID VARCHAR(30) NOT NULL,
-	Amount INT,
+    CREATE TABLE OutDonationTable (
+    RefNum INT NOT NULL,
+    DoneeID INT NOT NULL,
+    ItemID VARCHAR(30) NOT NULL,
+    Amount INT,
     FulfillDate TIMESTAMP,
-	PRIMARY KEY ( RefNum )
-		);"
+    PRIMARY KEY ( RefNum )
+    );"
 fi
 
 if [ $(mysql -N -s -u root -p${MYSQLROOTPASSWD} -e "select count(*) from information_schema.tables where table_schema='$MAINDB' and table_name='UserTable';") -eq 1 ]; then
@@ -124,8 +128,8 @@ if [ $(mysql -N -s -u root -p${MYSQLROOTPASSWD} -e "select count(*) from informa
 else
 	echo "Creating UserTable..."
 	mysql -uroot -p${MYSQLROOTPASSWD} $MAINDB -e "
-CREATE TABLE UserTable (
-	UserID INT NOT NULL AUTO_INCREMENT,
+    CREATE TABLE UserTable (
+    UserID INT NOT NULL AUTO_INCREMENT,
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
     State VARCHAR(50),
@@ -135,7 +139,7 @@ CREATE TABLE UserTable (
     AddressLine2 VARCHAR(100),
     CumulativeRecValue INT,
     Telephone VARCHAR(40),
-	Email VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
     PassHash VARCHAR(100) NOT NULL,
     PassSalt VARCHAR(50) NOT NULL,
     FlagAdmin INT NOT NULL,
@@ -150,7 +154,7 @@ CREATE TABLE UserTable (
     Active INT,
     lastTaxGenDate TIMESTAMP NOT NULL,
     PRIMARY KEY ( UserID )
-		);"
+    );"
 fi
 
 if [ $(mysql -N -s -u root -p${MYSQLROOTPASSWD} -e "select count(*) from information_schema.tables where table_schema='$MAINDB' and table_name='InventoryTable';") -eq 1 ]; then
@@ -158,14 +162,14 @@ if [ $(mysql -N -s -u root -p${MYSQLROOTPASSWD} -e "select count(*) from informa
 else
 	echo "Creating InventoryTable..."
 	mysql -uroot -p${MYSQLROOTPASSWD} $MAINDB -e "
-CREATE TABLE InventoryTable (
-	ItemID INT NOT NULL AUTO_INCREMENT,
+    CREATE TABLE InventoryTable (
+    ItemID INT NOT NULL AUTO_INCREMENT,
     Category INT,
     Name VARCHAR(30) NOT NULL,
     Amount INT,
     Threshold INT,
-		PRIMARY KEY ( ItemID )
-		);"
+    PRIMARY KEY ( ItemID )
+    );"
 fi
 
 if [ $(mysql -N -s -u root -p${MYSQLROOTPASSWD} -e "select count(*) from information_schema.tables where table_schema='$MAINDB' and table_name='CategoriesTable';") -eq 1 ]; then
@@ -173,11 +177,11 @@ if [ $(mysql -N -s -u root -p${MYSQLROOTPASSWD} -e "select count(*) from informa
 else
 	echo "Creating CategoriesTable..."
 	mysql -uroot -p${MYSQLROOTPASSWD} $MAINDB -e "
-	CREATE TABLE CategoriesTable (
-	CategoryNum INT NOT NULL AUTO_INCREMENT,
+    CREATE TABLE CategoriesTable (
+    CategoryNum INT NOT NULL AUTO_INCREMENT,
     Name VARCHAR(30) NOT NULL,
-		PRIMARY KEY ( CategoryNum )
-		);"
+    PRIMARY KEY ( CategoryNum )
+    );"
 fi
 
 echo -e "Mysql-server config complete.\n"
@@ -190,22 +194,24 @@ echo -e "Mysql-server config complete.\n"
 # Check if apache2 is installed
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' apache2|grep "install ok installed") || true
 echo Checking for apache2: $PKG_OK
+# Get apache2 if absent
 if [ "" == "$PKG_OK" ]; then
-# Get apache2
-echo ""
-apt-get -y install apache2 php5 php-pear php5-mysql
-a2enmod ssl php5
+  echo ""
+  apt-get -y install apache2 php5 php-pear php5-mysql > /dev/null 2&>1
+  a2enmod ssl php5
 fi
+# Configure virtualhost
 echo "Inserting new apache virtualhost..."
 cp ${WORKINGDIR}/apachetemplate.conf /etc/apache2/sites-available/${DNSNAME}.conf
 cd /etc/apache2/sites-available/
+# Inject values into virtualhost file
 sed -i "s#SITENAMETARGET#$DNSNAME#" ${DNSNAME}.conf
 sed -i "s#WEBROOTTARGET#$WEBROOT#" ${DNSNAME}.conf
+# Enable new site
 echo "Enabling apache host..."
 a2ensite ${DNSNAME}.conf
 service apache2 reload
 echo ""
-
 
 
 
@@ -220,7 +226,8 @@ cd $WEBROOT
 echo "Packing up old webroot..."
 tar czf oldwebroot-$DATE.tar.gz * --exclude="oldwebroot[.]*" || true
 rm -r html || true
-# TODO: delete old files after compress
+rm config.*
+
 echo "Installing new web components..."
 cp $WORKINGDIR/html $WEBROOT/ -r
 
@@ -234,26 +241,23 @@ git clone https://github.com/swiftmailer/swiftmailer.git
 mv swiftmailer swiftmailer-5.x
 
 
-echo -e "\nDone\n"
-
-
+# Substitute script values into config file
+# Caution: some fields have forward slashes
 cd $WEBROOT
 sed -i "s#\(mysql_addr=\).*#\1\"$DATABASEADDR\"#" config.ini
 sed -i "s/\(mysql_user=\).*/\1\"$MAINDBUSER\"/" config.ini
 sed -i "s/\(mysql_pass=\).*/\1\"$MYSQLROOTPASSWD\"/" config.ini
 sed -i "s/\(mysql_db=\).*/\1\"$MAINDB\"/" config.ini
-
-# Caution: webaddr has forward slashes
 sed -i "s#\(path_web=\).*#\1\"$WEBADDR\"#" config.ini
 sed -i "s/\(use_captchas=\).*/\1$CAPTCHAS/" config.ini
 sed -i "s/\(use_email_verification=\).*/\1$EMAILVER/" config.ini
-
 sed -i "s#\(time_zone=\).*#\1\"$TZ\"#" config.ini
-
 sed -i "s/\(No_Reply_email_address=\).*/\1\"$EMAILRELAYUSER\"/" config.ini
 sed -i "s/\(No_Reply_email_password=\).*/\1\"$EMAILRELAYPASSWD\"/" config.ini
-
 sed -i "s/\(contact_us_email=\).*/\1\"$CONTACTEMAILADDR\"/" config.ini
-
 sed -i "s/\(nonprofit_name=\).*/\1\"$COMPANYNAME\"/" config.ini
+
+
+echo -e "\nDone\n"
+
 
