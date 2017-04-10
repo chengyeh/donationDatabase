d56c41e84e431dec073a8e81c41a1c59815f2b95
Rusty Riedel <rusty.riedel@ku.edu>
2017-04-10 10:21:20 -0500

    Change nav bar highlight on the request history link to request.

--- html/doneeHistory.php
+++ html/doneeHistory.php
@@ -4,7 +4,7 @@ ini_set("display_errors", 1);
 
 session_start();
 
-$navbar_active = 'donate';
+$navbar_active = 'request';
 $navbar_title = 'Request History';
 include('layouts/navbar.php');
 require_once('helpers/mysqli.php');
